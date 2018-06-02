12e54f69709de82d5a13ae83eae6838c90373db5
Andrew Currier <curria@MLI-CURRIER.local>
2018-06-01 23:09:51 -0400

    Initial

---
+++ tipsy.js
@@ -0,0 +1,21 @@
+$(function() {
+	
+	$('[data-tip]').each(function(i) {
+		var $e = $(this),
+			tip = $e.data('tip'),
+			tipId = 'tip-' + i;
+
+		$e.removeAttr('data-tip').addClass('tip');
+
+		if ($e.is('a')) {
+			$e.attr('href', '#' + tipId);
+		}
+
+		$e.wrapInner('<div aria-describedby="' + tipId + '"></div>');
+		$e.append('<div role="tooltip" id="' + tipId + '">' + tip + '</div>');		
+		
+		$e.on('click', function() {
+			$(this).toggleClass('show-tip');
+		});		
+	});
+});
